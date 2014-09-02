981d4112d14b694a1abb54380d336e127372bfd9
Denis Lavrov <bahus.vel@gmail.com>
2014-09-02 23:14:36 +1200

    Added some useful generic methods.

--- LMS/src/SessionFactory/HibernateManager.java
+++ LMS/src/SessionFactory/HibernateManager.java
@@ -5,6 +5,8 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
+import java.util.List;
+
 /**
  * Created by denislavrov on 9/2/14.
  */
@@ -23,10 +25,10 @@ public class HibernateManager {
         }
     }
 
-    public class AutoSession implements AutoCloseable{
+    public class AutoTransaction implements AutoCloseable{
         public Session session = factory.openSession();
         public Transaction tx;
-        public AutoSession() {
+        public AutoTransaction() {
             try{
                 tx = session.beginTransaction();
             }
@@ -43,12 +45,37 @@ public class HibernateManager {
         }
     }
 
+    public class AutoSession implements AutoCloseable{
+        public Session session = factory.openSession();
+        @Override
+        public void close(){
+            session.close();
+        }
+    }
+
     public void addEntity(Object object) {
-        try (AutoSession as = new AutoSession()) {
-            as.session.save(object);
-            as.tx.commit();
-        } catch (MappingException e) {
+        try (AutoTransaction at = new AutoTransaction()) {
+            at.session.save(object);
+            at.tx.commit();
+        } catch (MappingException e) { // catch errors when user passes wrong objects
             System.err.println(object.getClass() + " Is not compatible with the database.");
         }
+    }
+
+    public List listEntities(Class entity){
+        List entities = null;
+        try(AutoSession as = new AutoSession()){
+            entities = as.session.createQuery("from " + entity.getSimpleName()).list();
+        }
+
+        return entities;
+    }
+
+    public AutoSession newAutoSession(){
+        return new AutoSession();
+    }
+
+    public AutoTransaction newAutoTransaction(){
+        return new AutoTransaction();
     }
 }
--- LMS/src/SessionFactory/Test.java
+++ LMS/src/SessionFactory/Test.java
@@ -1,17 +1,23 @@
 package SessionFactory;
 
-import Persistance.Book;
-
+import Persistance.Magazine;
 
 /**
  * Created by denislavrov on 9/2/14.
  */
 public class Test {
+    /**
+     * BIG DEAL !!!!
+     * methods in Session take arguments of serializable
+     * which really is just the primary key in the database.
+     * so just supplying the index is fine.
+     */
+
+
     public static void main(String[] args) {
         HibernateManager hm = new HibernateManager();
-        Book magazine = new Book();
-        magazine.setTitle("Test");
-        hm.addEntity(magazine);
-
+        for (Object o : hm.listEntities(Magazine.class)){
+            System.out.println(((Magazine)o).getTitle());
+        }
     }
 }
