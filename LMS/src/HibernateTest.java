f0db0a700333ceca5d7187dbfdbe23413637b551
Denis Lavrov <bahus.vel@gmail.com>
2014-09-02 17:43:01 +1200

    Hibernate is working.

--- LMS/src/HibernateTest.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by denislavrov on 9/2/14.
- */
-public class HibernateTest {
-
-}
--- LMS/src/Persistence/Book.java
+++ LMS/src/Persistence/Book.java
@@ -145,6 +145,11 @@ public class Book {
     }
 
     @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
